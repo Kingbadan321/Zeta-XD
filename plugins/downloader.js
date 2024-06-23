9af5f4abfd917d9d50032b635dda7361a129bee7
Badan Ser ꤸ <100457004+Kingbadan321@users.noreply.github.com>
2024-06-23 17:28:32 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -347,7 +347,6 @@ command(
     },
     async (message, match, m) => {
     if(process.cwd() !== "/root/zeta") return message.reply("use og version myra https://github.com/Kingbadan321/Zeta-XD");
-match = match || message.reply_message.text;
 	if(!match) return await message.reply(`*_Need A Text_*\n*eg:- .dalle a modified gtr in garage*`);
 await message.sendFromUrl(`https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${match}`, {caption: (X.CAPTION)})
 });
